fe0e2e0abf557ed765dc75abe0ac8156ac935295
xiuyangzhe <769865744@qq.com>
2023-05-18 23:24:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tuya_smart_home_sdk
+ 涂鸦智能生活Flutter 第三方插件
---
+++ lib/tuya_smart_home_sdk.dart
@@ -0,0 +1,7 @@
+library tuya_smart_home_sdk;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
